fa98f468bb876125b2539f801b8b3f5cc20d9cbf
zye7ert <edgarxot@gmail.com>
2021-08-25 23:12:05 -0500

    Forming the path

---
+++ pathcat.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * pathcat - concatenates a command with a path
+ * @path: path to add
+ * @command: command
+ *
+ * Return: buffer
+ */
+
+char *pathcat(char *path, char *command)
+{
+        char *buffer;
+        size_t a = 0, b = 0;
+
+        if (command == 0)
+                command = "";
+        if (path == 0)
+                path = "";
+        buffer = malloc(sizeof(char) * _strlen(path) + _strlen(command) + 2);
+        if (buffer == NULL)
+                return (NULL);
+        while (path[a])
+        {
+                buffer[a] = path[a];
+                a++;
+        }
+        if (path[a - 1] != '/')
+        {
+                buffer[a] = '/';
+                a++;
+        }
+        while (command[b])
+        {
+                buffer[a + b] = command[b];
+                b++;
+        }
+        buffer[a + b] = '\0';
+        return (buffer);
+}
