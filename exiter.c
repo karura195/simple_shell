1214e4c69fa6bfe69479172b9b7b522c54e5cba0
zye7ert <edgarxot@gmail.com>
2021-08-25 23:32:53 -0500

    Frees memory

---
+++ exiter.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * exiter - frees memory and exits the program
+ * @arr: array or args
+ * @line: line read
+ *
+ * Return: void
+ */
+
+void exiter(char **arr, char *line)
+{
+        free(line);
+        free_args(arr);
+        exit(0);
+}
